8f6b53e3a1b1603af6009eabe0dcab7da9c706e2
Andrei <0212anponamarev@gmail.com>
2024-03-03 21:56:27 +0100

    add tests

--- src/app/core/models/family.model.ts
+++ src/app/core/models/family.model.ts
@@ -2,7 +2,6 @@ import { BabyProfile } from './baby-profile.model';
 import { ParentProfile } from './parent-profile.model';
 
 export type Family = {
-  id: string;
   parents: ParentProfile[];
   babies: BabyProfile[];
 };
--- src/app/core/services/family.service.ts
+++ src/app/core/services/family.service.ts
@@ -1,12 +1,62 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import {
+  CollectionReference,
+  Firestore,
+  addDoc,
+  arrayRemove,
+  arrayUnion,
+  collection,
+  deleteDoc,
+  doc,
+  updateDoc,
+} from '@angular/fire/firestore';
+import { DbTitle } from '../models/enums/db-title.enum';
+import { Family } from '../models/family.model';
+import { ParentProfile } from '../models/parent-profile.model';
+import { BabyProfile } from '../models/baby-profile.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FamilyService {
-  //create
-  //join
-  //invite
-  //leave
-  //remoe
+  #firestore = inject(Firestore);
+  #familiesCollection: CollectionReference = collection(
+    this.#firestore,
+    DbTitle.FAMILIES
+  );
+
+  async createFamily(newFamily: Family) {
+    await addDoc(this.#familiesCollection, newFamily);
+  }
+
+  async join(id: string, parent: ParentProfile) {
+    const ref = doc(this.#familiesCollection, id);
+    await updateDoc(ref, { parents: arrayUnion(parent) });
+  }
+
+  async leave(id: string, parent: ParentProfile) {
+    const ref = doc(this.#familiesCollection, id);
+    await updateDoc(ref, { parents: arrayRemove(parent) });
+  }
+
+  async remove(id: string) {
+    const ref = doc(this.#familiesCollection, id);
+    await deleteDoc(ref);
+  }
+
+  async addBaby(id: string, baby: BabyProfile) {
+    const ref = doc(this.#familiesCollection, id);
+    await updateDoc(ref, { babies: arrayUnion(baby) });
+  }
+
+  removeBaby(id: string, baby: BabyProfile) {
+    const ref = doc(this.#familiesCollection, id);
+    updateDoc(ref, { babies: arrayRemove(baby) });
+  }
+
+  async collectionIntoCollection(id: string, parent: any) {
+    const ref = doc(this.#familiesCollection, id);
+    const elem = collection(ref, 'babies');
+    await addDoc(elem, parent);
+  }
 }
